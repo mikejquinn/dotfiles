f1a3de82f27e73e72b57320f169bdc099f7e95d1
Michael Quinn <mikeq@liftoff.io>
2015-12-02 07:15:43 -0800

    Update hammerspoon config with some movement shortcuts

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,7 +5,13 @@ local hotkey = require "hs.hotkey"
 local screen = require "hs.screen"
 local window = require "hs.window"
 
+-- the window move animations are annoying
 window.animationDuration = 0
+
+local mash      = {"ctrl", "cmd"}
+local mashAlt   = {"ctrl", "cmd", "alt"}
+local mashShift = {"ctrl", "cmd", "shift"}
+local mashAll   = {"ctrl", "cmd", "alt", "shift"}
 
 -- Window movements
 
@@ -20,49 +26,78 @@ local gridset = function(frame)
    end
 end
 
+-- Set up grid
+
 local gridW = 6
 local gridH = 8
+
+grid.setGrid(hs.geometry.size(gridW, gridH))
+   .setMargins(hs.geometry.point(0, 0))
+
+-- move windows to discrete positions
 
 local goleft = {x=0, y=0, w=gridW/2, h=gridH}
 local goright = {x=gridW/2, y=0, w=gridW/2, h=gridH}
 local gotop = {x=0, y=0, w=gridW, h=gridH/2}
 local gobottom = {x=0, y=gridH/2, w=gridW, h=gridH/2}
 
-grid.setGrid(hs.geometry.size(gridW, gridH))
-   .setMargins(hs.geometry.point(0, 0))
+local goupleft = {x=0, y=0, w=gridW/2, h=gridH/2}
+local goupright = {x=gridW/2, y=0, w=gridW/2, h=gridH/2}
+local godownleft = {x=0, y=gridH/2, w=gridW/2, h=gridH/2}
+local godownright = {x=gridW/2, y=gridH/2, w=gridW/2, h=gridH/2}
 
 local movements = {
-   {mod={"ctrl", "cmd"}, key="Left", fn=gridset(goleft)},
-   {mod={"ctrl", "cmd"}, key="Up", fn=gridset(gotop)},
-   {mod={"ctrl", "cmd"}, key="Down", fn=gridset(gobottom)},
-   {mod={"ctrl", "cmd"}, key="Right", fn=gridset(goright)},
-   {mod={"ctrl", "cmd"}, key="m", fn=grid.maximizeWindow}
+   {mod=mashShift, key="H", fn=gridset(goleft)},
+   {mod=mashShift, key="K", fn=gridset(gotop)},
+   {mod=mashShift, key="J", fn=gridset(gobottom)},
+   {mod=mashShift, key="L", fn=gridset(goright)},
+   {mod=mashAll, key="U", fn=gridset(goupleft)},
+   {mod=mashAll, key="I", fn=gridset(goupright)},
+   {mod=mashAll, key="J", fn=gridset(godownleft)},
+   {mod=mashAll, key="K", fn=gridset(godownright)},
+   {mod=mashShift, key="M", fn=grid.maximizeWindow}
 }
 
 fnutils.each(movements, function(m)
   hotkey.bind(m.mod, m.key, m.fn)
 end)
 
--- Application focus hotkeys
+-- move windows
+hotkey.bind(mashShift, "Left", hs.grid.pushWindowLeft)
+hotkey.bind(mashShift, "Down", hs.grid.pushWindowDown)
+hotkey.bind(mashShift, "Up", hs.grid.pushWindowUp)
+hotkey.bind(mashShift, "Right", hs.grid.pushWindowRight)
 
-local hotkeys = {
-   {key="J", name="Google Chrome"},
-   {key="L", name="Emacs"},
-   {key="K", name="iTerm"},
-   {key="U", name="GitX"},
-   {key="I", name="Adium"},
-   {key="Y", name="Messages"},
-   {key="O", name="Things"}
+-- resize windows
+hotkey.bind(mashShift, "Y", hs.grid.resizeWindowThinner)
+hotkey.bind(mashShift, "U", hs.grid.resizeWindowTaller)
+hotkey.bind(mashShift, "I", hs.grid.resizeWindowShorter)
+hotkey.bind(mashShift, "O", hs.grid.resizeWindowWider)
+
+-- multi monitor
+hotkey.bind(mashShift, "N", hs.grid.pushWindowNextScreen)
+hotkey.bind(mashShift, "P", hs.grid.pushWindowPrevScreen)
+
+-- Application focus hotkeys
+local appShortcuts = {
+   j = "Google Chrome",
+   l = "Emacs",
+   k = "iTerm",
+   u = "GitX",
+   i = "Adium",
+   y = "Messages",
+   o = "Things"
 }
 
-fnutils.each(hotkeys, function(k)
-  hotkey.bind({"cmd", "ctrl"}, k.key, function()
-    local app = hs.application.get(k.name)
+for key, appName in pairs(appShortcuts) do
+  hotkey.bind(mash, key, function()
+    local app = hs.application.get(appName)
     if app then app:activate() end
   end)
-end)
+end
 
-hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+-- Reload hammerspoon config
+hotkey.bind(mashAlt, "R", function()
   hs.reload()
 end)
 
