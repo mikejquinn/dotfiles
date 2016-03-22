ef7f0c886aa1b3b8293efbc3edd42b3d06f5d1d5
Michael Quinn <mikeq@liftoff.io>
2016-03-22 11:12:52 -0700

    Window layout tweaks

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,14 +4,41 @@ local grid = require "hs.grid"
 local hotkey = require "hs.hotkey"
 local screen = require "hs.screen"
 local window = require "hs.window"
+local layout = require "hs.layout"
 
--- the window move animations are annoying
+local log = hs.logger.new("init", "debug")
+
+-- The window move animations are annoying
+
 window.animationDuration = 0
 
 local mash      = {"ctrl", "cmd"}
 local mashAlt   = {"ctrl", "cmd", "alt"}
 local mashShift = {"ctrl", "cmd", "shift"}
 local mashAll   = {"ctrl", "cmd", "alt", "shift"}
+
+-- Reload hammerspoon config
+
+hotkey.bind(mashAlt, "R", function()
+  hs.reload()
+end)
+
+-- Load all screens and sort them from left to right
+
+local getSortedScreens = function()
+  local screens = hs.screen.allScreens()
+  table.sort(screens, function(screen1, screen2)
+    x1, y1 = screen1:position()
+    x2, y2 = screen2:position()
+    return x1 < x2
+  end)
+
+  for i, s in ipairs(screens) do
+    log.d(i)
+  end
+
+  return screens
+end
 
 -- Window movements
 
@@ -34,7 +61,7 @@ local gridH = 8
 grid.setGrid(hs.geometry.size(gridW, gridH))
    .setMargins(hs.geometry.point(0, 0))
 
--- move windows to discrete positions
+-- Move windows to discrete positions
 
 local goleft = {x=0, y=0, w=gridW/2, h=gridH}
 local goright = {x=gridW/2, y=0, w=gridW/2, h=gridH}
@@ -45,6 +72,13 @@ local goupleft = {x=0, y=0, w=gridW/2, h=gridH/2}
 local goupright = {x=gridW/2, y=0, w=gridW/2, h=gridH/2}
 local godownleft = {x=0, y=gridH/2, w=gridW/2, h=gridH/2}
 local godownright = {x=gridW/2, y=gridH/2, w=gridW/2, h=gridH/2}
+
+function gridToFrame(screen, grid)
+  local screenFrame = screen:frame()
+  local cellW = screenFrame.w / gridW
+  local cellH = screenFrame.h / gridH
+  return hs.geometry.rect(cellW * grid.x, cellH * grid.y, cellW * grid.w, cellH * grid.h)
+end
 
 local movements = {
    {mod=mashShift, key="H", fn=gridset(goleft)},
@@ -96,9 +130,24 @@ for key, appName in pairs(appShortcuts) do
   end)
 end
 
--- Reload hammerspoon config
-hotkey.bind(mashAlt, "R", function()
-  hs.reload()
-end)
+-- Work layout
+
+function layoutForWork()
+  local screens = getSortedScreens()
+  local workLayout = {
+    {"Google Chrome", nil, screens[1], {x=0, y=0, w=0.5, h=1}, nil, nil},
+    {"iTerm", nil, screens[1], {x=0.5, y=0, w=0.5, h=1}, nil, nil},
+    {"Slack", nil, screens[1], {x=0.5, y=0, w=0.5, h=1}, nil, nil},
+    {"Emacs", nil, screens[2], {x=0, y=0, w=1, h=1}, nil, nil},
+    {"Google Chrome", "Google Hangouts", screens[3], {x=0, y=0, w=0.5, h=1}, nil, nil},
+    {"Messages", nil, screens[3], {x=0.5, y=0, w=0.5, h=1}, nil, nil},
+    {"GitX", nil, screens[1], {x=0.5, y=0, w=0.5, h=1}, nil, nil},
+    {"Calendar", nil, screens[3], {x=0, y=0, w=1, h=1}, nil, nil}
+  }
+
+  hs.layout.apply(workLayout)
+end
+
+hotkey.bind(mashAll, "W", layoutForWork)
 
 hs.alert.show("Config loaded")
