97653e4b08c2ed3002b9177a1415e09851839aad
Michael Quinn <mike@liftoff.io>
2016-11-21 09:56:04 -0800

    [profiles.clj] Upgrade cider to 0.13.0

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]]
-        :dependencies [[com.cemerick/piggieback "0.2.1"]
-                       [org.clojure/tools.nrepl "0.2.10"]]
-        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
+{:user {:plugins [[cider/cider-nrepl "0.13.0"]]}
  :auth {:repository-auth {#"https://clojars.org/repo" {:username "mikeq"
                                                        :password "k9o3fs68W36zE82Le"}}}}
-                  ;[lein-pprint "1.1.1"]
